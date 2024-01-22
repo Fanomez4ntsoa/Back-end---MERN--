c557822fddfe2725099fa74aa00190db75e8f13f
Voary Fanomezantsoa <voary.fanomezantsoa@gmail.com>
2024-01-22 19:31:59 +0100

    feat : add and setting up middleware & error management & route configuration & Environment variable configuration & database configuration

---
+++ README.md
@@ -0,0 +1,6 @@
+1er TODO List :
+- Ajout Middleware ✅
+- Gestion des erreurs ✅
+- Configuration routes ✅
+- Configuration du variable d'environnement ✅
+- Configuration du base donnée ✅
---
+++ server.js
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const databaseConnection = require('./config/connection');
+
+dotenv.config();
+// Connection base de donnée
+databaseConnection();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV === 'development';
+
+if(ENV) {
+    app.use(morgan('dev'));
+}
+
+// Middleware pour gérer le corps des requêtes JSOn
+app.use(express.json());
+app.use(notFound)
+app.use(errorHandler)
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome on the project MERN !')
+});
+
+const static = path.resolve()
+app.use('/public/uploads', express.static(path.join(static, '/public/uploads')))
+
+// Ecoute du serveur sur le port spécifique
+app.listen(
+    PORT, () => {
+    console.log(`Le serveur est en écoute sur le port ${PORT}`.yellow.bold)
+});
